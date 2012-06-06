26187391da2ab365489daff759f5b9ad85d76ecf
maki_t2nd <maki_t@maki-o.net>
2012-06-06 14:12:11 +0900

    左右ボタン,キャプション,何番目の画像とかの機能追加

--- js/jquery.fadeImage.js
+++ js/jquery.fadeImage.js
@@ -1,6 +1,6 @@
 /*
 
-lastupdate:2012-02-22
+lastupdate:2012-06-06
 
 */
 
@@ -13,6 +13,11 @@ lastupdate:2012-02-22
 			interval : 5000,
 			fadeSpeed: 1000,
 			canvas   : 'canvas',
+			control  : 'control',
+			caption  : 'caption',
+			capDisp  : true,
+			amount   : 'amount',
+			amtDisp  : true,
 			current  : 'cur',
 			cross    : true
 		}, opt);
@@ -21,36 +26,95 @@ lastupdate:2012-02-22
 			var timer;
 			var $this    = $(this);
 			var $canvas  = $this.find('.'+opt.canvas);
-			var length   = $canvas.find('li').size()-1;
+			var $control = $this.find('.'+opt.control);
+			var $caption = $this.find('.'+opt.caption);
+			var $amount  = $this.find('.'+opt.amount);
+			var $btnPrev = $control.find('.prev');
+			var $btnNext = $control.find('.next');
+			var _len     = $canvas.find('li').size();
+			var length   = _len - 1;
+			var dataObj  = {'all':(_len > 9) ? _len : '0'+_len};
+			var tpl      = $amount.html();
 			
-			function fadeImage(){
-				var $next = ($canvas.find('li').index($canvas.find('.'+opt.current)) != length) ? $canvas.find('.'+opt.current).next() : $canvas.find('li:first');
+			function fadeImage(flag){
+				var flag = flag || false;
+				var $next;
+				var $nextCap;
+				var curIndex = $canvas.find('li').index($canvas.find('.'+opt.current));
+				var curCapIndex = $caption.find('li').index($caption.find('.'+opt.current));
+				var nextIndex;
+				
+				if(flag){
+					$next = (curIndex != 0) ? $canvas.find('.'+opt.current).prev() : $canvas.find('li:last');
+					if(opt.capDisp) $nextCap = (curCapIndex != 0) ? $caption.find('.'+opt.current).prev() : $caption.find('li:last');
+				}else{
+					$next = (curIndex != length) ? $canvas.find('.'+opt.current).next() : $canvas.find('li:first');
+					if(opt.capDisp) $nextCap = (curCapIndex != length) ? $caption.find('.'+opt.current).next() : $caption.find('li:first');
+				}
+				
+				nextIndex = $canvas.find('li').index($next) + 1;
+				dataObj.to = (nextIndex > 9) ? nextIndex : '0' + nextIndex;
+							
 				if(opt.cross){
 					$canvas.find('.'+opt.current).not(':animated').animate(
 						{opacity:'0'},
 						{duration:opt.fadeSpeed,queue:false}
 					);
-					$next.not(':animated').animate(
-						{opacity:'1'},
-						{duration:opt.fadeSpeed,queue:false,complete:
-							function(){
-								$canvas.find('.'+opt.current).removeClass(opt.current);
-								$next.addClass(opt.current);
-							}
-						}
-					);
+					
+					if(opt.capDisp){
+						$caption.find('.'+opt.current).not(':animated').animate(
+							{opacity:'0'},
+							{duration:opt.fadeSpeed,queue:false}
+						);
+					}
+					
+					nextFade();
+					
 				}else{
 					$canvas.find('.'+opt.current).css({opacity:0});
-					$next.not(':animated').animate(
+					if(opt.capDisp) $caption.find('.'+opt.current).css({opacity:0});
+					
+					nextFade();
+				}
+			}
+			
+			function nextFade(){
+				$next.not(':animated').animate(
+					{opacity:'1'},
+					{duration:opt.fadeSpeed,queue:false,complete:
+						function(){
+							$canvas.find('.'+opt.current).removeClass(opt.current);
+							$next.addClass(opt.current);
+							if(opt.amtDisp) setTpl();
+						}
+					}
+				);
+				
+				if(opt.capDisp){
+					$nextCap.not(':animated').animate(
 						{opacity:'1'},
 						{duration:opt.fadeSpeed,queue:false,complete:
 							function(){
-								$canvas.find('.'+opt.current).removeClass(opt.current);
-								$next.addClass(opt.current);
+								$caption.find('.'+opt.current).removeClass(opt.current);
+								$nextCap.addClass(opt.current);
 							}
 						}
 					);
 				}
+			}
+			
+			function setTpl(){
+				var src = tpl.replace(/{#(\w+)}/g,function(m,key){
+					return dataObj[key] || '';
+				});
+				
+				$amount.html(src);
+			}
+			
+			function startFade(){
+				timer = setInterval(function(){
+					fadeImage();
+				},opt.interval);
 			}
 			
 			$canvas.find('li:not(:first)').css({
@@ -59,9 +123,41 @@ lastupdate:2012-02-22
 			
 			$canvas.find('li:first').addClass(opt.current);
 			
-			timer = setInterval(function(){
+			if(opt.capDisp){
+				$caption.find('li:not(:first)').css({
+					opacity:0
+				});
+				$caption.find('li:first').addClass(opt.current);
+			}
+			
+			if(opt.amtDisp){
+				dataObj.to = '01';
+				setTpl();
+			}
+			
+			startFade();
+			
+			$btnPrev.bind('click',function(){
+				clearInterval(timer);
+				fadeImage(true);
+				startFade();
+				return false;
+			});
+			
+			$btnNext.bind('click',function(){
+				clearInterval(timer);
 				fadeImage();
-			},opt.interval);
+				startFade();
+				return false;
+			});
+			
+			$canvas.bind('mouseover',function(){
+				clearInterval(timer);
+			});
+			
+			$canvas.bind('mouseout',function(){
+				startFade();
+			});
 			
 		});
 		return this;
