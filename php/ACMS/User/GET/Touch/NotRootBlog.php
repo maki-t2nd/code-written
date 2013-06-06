915b9393209b4d9d08072b1dd806675fb0ef25fd
maki-t2nd <maki_t@maki-o.net>
2013-06-06 10:28:33 +0900

    require削除

--- php/ACMS/User/GET/Touch/NotRootBlog.php
+++ php/ACMS/User/GET/Touch/NotRootBlog.php
@@ -1,11 +1,9 @@
 <?php
 
-require_once ACMS_LIB_DIR.'GET.php';
-
 class ACMS_User_GET_Touch_NotRootBlog extends ACMS_GET
 {
     function get()
     {
-		return (BID == 1) ? false : $this->tpl ;
+    return (BID == 1) ? false : $this->tpl ;
     }
 }
--- php/ACMS/User/GET/Touch/RootBlog.php
+++ php/ACMS/User/GET/Touch/RootBlog.php
@@ -1,11 +1,9 @@
 <?php
 
-require_once ACMS_LIB_DIR.'GET.php';
-
 class ACMS_User_GET_Touch_RootBlog extends ACMS_GET
 {
     function get()
     {
-		return (BID == 1) ? $this->tpl : false ;
+    return (BID == 1) ? $this->tpl : false ;
     }
 }
